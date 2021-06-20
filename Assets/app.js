9a326f620c7fbaa97b0319eb45ff91abb877d7be
Malik Umair Khalid <fighteromega3462@gmail.com>
2021-06-21 00:55:42 +0500

    Completed Improved

--- Assets/app.js
+++ Assets/app.js
@@ -1,21 +1,18 @@
 
 var x = setInterval(function ramzanRemainingTime(){
-var date = new Date().getTime()
-var date2 = new Date ('july 19, 2021').getTime()
-var difference = date2 - date
-var days = Math.round(difference / 8.64e+7)
-// document.write(days)
-var weeks = Math.round(difference / 6.048e+8)
-// document.write('</br>' ,  weeks)
-var minuts = Math.round(difference / 60000)
-var hours = Math.round(difference / 3.6e+6)
-// document.write('</br>' , minuts)
-var seconds = Math.round(difference / 1000)
-// document.write('</br>' , seconds)
+var now = new Date().getTime()
+var countDownDate = new Date ('july 19, 2021').getTime()
+var timeleft = countDownDate - now; var days = Math.floor(timeleft / (1000 * 60 * 60 * 24))
+var days = Math.round(timeleft / 8.64e+7)
+var weeks = Math.round(timeleft / 6.048e+8)
+var minuts = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60))
+var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); 
+var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 var mainParagraph = document.getElementById("mainPara")
 mainParagraph.innerHTML = days + " &nbsp:&nbsp " + hours + " &nbsp:&nbsp " + minuts + " &nbsp:&nbsp " + seconds;
 var mainParagraph2 = document.getElementById("para3")
-mainParagraph2.innerHTML = 'Days' + "|" + "&nbspHours&nbsp " + "|" + "&nbsp&nbspMinuts &nbsp&nbsp&nbsp" + "|" + "&nbsp&nbspSeconds"
+mainParagraph2.innerHTML = 'Days' + "|" + "Hour" + "|" + "Minuts" + "|" + "Seconds"
+mainParagraph.style.wordSpacing = '10'
 
 var paragraph = document.getElementById('para')
 paragraph.innerHTML = "In Bakron Ko Zibba Hona ma " + days + " Din " + weeks + " Hafta " + minuts + " Minuts " + seconds + " seconds Baake Hain"
